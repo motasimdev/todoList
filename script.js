56351c7869ed56c22920197cdba3fbdf0793d299
motasimdev <themotasimdev@gmail.com>
2025-11-09 00:01:25 +0600

    asdfsf

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const todoInput = document.getElementById("todoInput");
 const submitBtn = document.getElementById("submitBtn");
 const todoListItem = document.getElementById("todoListItem");
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener("click", addTodo);
+
+todoInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addTodo();
+  }
+});
+
+function addTodo() {
   const inputVal = todoInput.value;
 
   //   =============li create========
   const li = document.createElement("li");
   li.textContent = inputVal;
-  li.className = "text-lg font-medium bg-white py-1 px-4 rounded-2xl flex items-center justify-between mb-2";
+  li.className =
+    "text-lg font-medium bg-white py-1 px-4 rounded-2xl flex items-center justify-between mb-2";
   //   =============li create========
   todoListItem.appendChild(li);
 
@@ -16,16 +25,16 @@ submitBtn.addEventListener("click", () => {
   const delBtn = document.createElement("button");
   delBtn.textContent = "Delete";
   delBtn.className = "bg-red-600 py-2 px-5 rounded-2xl cursor-pointer";
-  delBtn.addEventListener("click", ()=>{
-    li.remove()
-  })
+  delBtn.addEventListener("click", () => {
+    li.remove();
+  });
   //   =============delete btn create========
 
-  li.appendChild(delBtn)
+  li.appendChild(delBtn);
 
   todoInput.value = "";
 
   if (!inputVal) {
     alert("vai khali ken?");
   }
-});
+}
