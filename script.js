275e23a5785fca3bd3253a0c3954e09d2e0fd19a
motasimdev <themotasimdev@gmail.com>
2025-11-08 23:25:39 +0600

    sdafsad

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ const todoListItem = document.getElementById("todoListItem");
 
 submitBtn.addEventListener("click", () => {
   const inputVal = todoInput.value;
+
+  //   =============li create========
   const li = document.createElement("li");
   li.textContent = inputVal;
-  todoListItem.appendChild(li)
-  todoInput.value=''
-  if (!inputVal) {
-    alert("vai khali ken?")
-  }
+  li.className = "text-lg font-medium bg-white py-1 px-4 rounded-2xl flex items-center justify-between";
+  //   =============li create========
+  todoListItem.appendChild(li);
 
+  //   =============delete btn create========
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.className = "bg-red-600 py-2 px-5 rounded-2xl cursor-pointer";
+  //   =============delete btn create========
+  li.appendChild(delBtn)
+
+  todoInput.value = "";
+
+  if (!inputVal) {
+    alert("vai khali ken?");
+  }
 });
