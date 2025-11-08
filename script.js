eadae76ad695c6153bd237059d71817c036d10bb
motasimdev <themotasimdev@gmail.com>
2025-11-08 23:15:40 +0600

    adfafsd

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const submitBtn = document.getElementById("submitBtn");
 const todoListItem = document.getElementById("todoListItem");
 
 submitBtn.addEventListener("click", () => {
-    
-    const inputVal = todoInput.value;    
-    todoListItem.innerHTML=inputVal
-    
-    
+  const inputVal = todoInput.value;
+  const li = document.createElement("li");
+  li.textContent = inputVal;
+  todoListItem.appendChild(li)
+  todoInput.value=''
+  if (!inputVal) {
+    alert("vai khali ken?")
+  }
+
 });
