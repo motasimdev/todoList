fb33b8d457d0e837e7984fca388f28209b9085ee
motasimdev <themotasimdev@gmail.com>
2025-11-08 23:27:09 +0600

    asdfsadf

--- script.js
+++ script.js
@@ -16,7 +16,11 @@ submitBtn.addEventListener("click", () => {
   const delBtn = document.createElement("button");
   delBtn.textContent = "Delete";
   delBtn.className = "bg-red-600 py-2 px-5 rounded-2xl cursor-pointer";
+  delBtn.addEventListener("click", ()=>{
+    li.remove()
+  })
   //   =============delete btn create========
+
   li.appendChild(delBtn)
 
   todoInput.value = "";
